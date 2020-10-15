b32935c84474e90de09ecce6d164d8d2b9017474
Ji Hyun Bak <jihyun.bak@gmail.com>
2020-10-15 16:08:46 -0700

    add setup and environment tools

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# rec-header-to-yaml
+# rec_header_to_yaml
+
+Effort to extract and collect information from the experimental data
+(mostly from .rec header information)
+to build a draft of the metadata yaml file for `rec_to_nwb`.
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rec_header_to_yaml',
+    version='0.1.0.dev0',
+    description=('extract rec header information to draft a metadata.yaml'),
+    author='Ji Hyun Bak',
+    author_email='jihyun.bak@gmail.com',
+    url='https://github.com/jihyunbak/rec_header_to_yaml',
+    packages=find_packages(),
+)
+
