781ae9fb477cbcd4257e041b73ea435bc263e253
Ji Hyun Bak <jihyun.bak@gmail.com>
2020-10-18 15:15:59 -0700

    refactor probe detection

--- rec_header_to_yaml/metadata.py
+++ rec_header_to_yaml/metadata.py
@@ -433,6 +433,20 @@ class NWBMetadataHelper():
         xml_data = self.get_config_from_header()
         ntrodes_config = self.extract_ntrodes_info(xml_data)
         
+        # should be sorted from probes with fewer # channels
+        probes_used = [
+            {
+                'ch_per_probe': 4,
+                'ch_per_shank': 4,
+                'device_type': 'tetrode_12.5'
+            },
+            {
+                'ch_per_probe': 32,
+                'ch_per_shank': 16,
+                'device_type': '32c-2s8mm6cm-20um-40um-dl'
+            }
+        ]
+        
         electrode_groups = []
         group_id = 0
         ch_cnt = 0
@@ -441,27 +455,48 @@ class NWBMetadataHelper():
                 num_channels = sum([1 for k in ntrode['map']])
             except KeyError:
                 continue
-            if num_channels <= 4:
-                group = {'id': group_id, 'device_type': 'tetrode_12.5'}
-                electrode_groups.append(group)
-                ntrode['electrode_group'] = group_id
-                group_id += 1
-                ch_id_base = 0
-            elif num_channels <= 16:
-                # this is a part of a 32-channel probe
-                # in general this should be more flexible
-                if ch_cnt + num_channels > 32:
-                    # assign to a new electrode group
-                    group_id += 1
-                    ch_cnt = num_channels
-                    ch_id_base = 0
-                else:
-                    ch_id_base = ch_cnt
-                    ch_cnt += num_channels
-                group = {'id': group_id, 'device_type': '32c-2s8mm6cm-20um-40um-dl'}
-                electrode_groups.append(group)
-                ntrode['electrode_group'] = group_id
-            else:
+            # if num_channels <= 4:
+            #     group = {'id': group_id, 'device_type': 'tetrode_12.5'}
+            #     electrode_groups.append(group)
+            #     ntrode['electrode_group'] = group_id
+            #     group_id += 1
+            #     ch_id_base = 0
+            # elif num_channels <= 16:
+            #     # this is a part of a 32-channel probe
+            #     # in general this should be more flexible
+            #     if ch_cnt + num_channels > 32:
+            #         # assign to a new electrode group
+            #         group = {'id': group_id, 'device_type': '32c-2s8mm6cm-20um-40um-dl'}
+            #         electrode_groups.append(group)
+            #         group_id += 1
+            #         ch_cnt = num_channels
+            #         ch_id_base = 0
+            #     else:
+            #         ch_id_base = ch_cnt
+            #         ch_cnt += num_channels
+            #     ntrode['electrode_group'] = group_id
+            # else:
+            #     raise RuntimeError('unknown shank type')
+            # self._remap_channels(ntrode, base=ch_id_base)
+
+            found_probe = False
+            for probe in probes_used:
+                if num_channels <= probe['ch_per_shank']:
+                    if ch_cnt + num_channels > probe['ch_per_probe']:
+                        # assign to a new electrode group
+                        group = {'id': group_id,
+                                 'device_type': probe['device_type']}
+                        electrode_groups.append(group)
+                        group_id += 1
+                        ch_cnt = num_channels
+                        ch_id_base = 0
+                    else:
+                        ch_id_base = ch_cnt
+                        ch_cnt += num_channels
+                    ntrode['electrode_group'] = group_id
+                    found_probe = True
+                    break
+            if not found_probe:
                 raise RuntimeError('unknown shank type')
             self._remap_channels(ntrode, base=ch_id_base)
                 
